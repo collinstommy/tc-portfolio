f0f0a25dcfbc64297af03c5ddf52ac60202fd45f
Thomas Collins <tcollins@hubspot.com>
2021-07-26 21:13:14 +0100

    add work + education

---
+++ src/components/skills.svelte
@@ -0,0 +1,34 @@
+<script>
+	import i18n from '../config/i18n';
+
+	const {
+		tech: { production, side }
+	} = i18n;
+</script>
+
+<style>
+</style>
+
+<div class="py-10 flex flex-col items-start">
+	<h2 class="section-heading">Technolgy</h2>
+  <div class="grid inline-grid grid-cols-2 mt-8 gap-8">
+    <div>
+      <h3>Production</h3>
+      <ul>
+        {#each production as item}
+           <li>{item}</li>
+        {/each}
+      </ul>
+    </div>
+    <div>
+      <h3>Side Projects</h3>
+      <ul>
+        {#each side as item}
+           <li>{item}</li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</div>
+
+
