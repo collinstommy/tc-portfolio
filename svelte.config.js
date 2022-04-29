7afcfe365917b3b364ff09a62c6e8339945d0f03
Tom Collins <tomascollins@gmail.com>
2022-04-29 17:57:29 +0100

    adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 import adapterStatic from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapterStatic({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: adapter()
+		// adapter: adapterStatic({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null
+		// })
 	},
 	preprocess: [
 		preprocess({
