a3286066b41ffd43c7f9c980e8abd95b706cedf9
Tom Collins <tomascollins@gmail.com>
2022-04-29 15:42:05 +0100

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import preprocess from 'svelte-preprocess';
-/** @type {import('@sveltejs/kit').Config} */
 import adapterStatic from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapterStatic({
